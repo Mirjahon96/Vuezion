f0b263816778d71d8c023d1e2f9a52acede107e9
Mirjahon96 <auden.05@mail.ru>
2020-05-06 02:32:57 +0500

    finishing contact us page

---
+++ src/components/AppInput.vue
@@ -0,0 +1,58 @@
+<template functional>
+  <div id="input-field">
+    <div class="field">
+      <label class="label has-text-weight-medium has-text-grey">{{ props.label }}</label>
+      <div class="control">
+        <input
+          v-if="props.textarea == false"
+          :class="props.styles"
+          class="input custom-input"
+          :type="props.type"
+          :placeholder="props.placeholder"
+        />
+
+        <textarea
+          v-else-if="props.textarea"
+          :class="props.styles"
+          class="textarea custom-textarea"
+          :placeholder="props.placeholder"
+        ></textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  functional: true,
+  name: "AppInput",
+  props: {
+    textarea: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    label: {
+      type: String,
+      required: false,
+      default: "label"
+    },
+    styles: {
+      type: String,
+      required: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "text"
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: "placeholder"
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -23,17 +23,120 @@
     </div>
 
     <!-- Contact Form -->
-    <section></section>
+    <section class="contact-form pb-40">
+      <div class="container">
+        <div class="columns">
+          <!-- 1 column -->
+          <div class="column is-6">
+            <h3 class="display-3 has-text-weight-bold">Get in Touch</h3>
+            <p class="has-text-weight-medium mb-30">It is a long established fact</p>
+            <form>
+              <div class="columns is-multiline">
+                <div class="column is-6">
+                  <!-- 1 field -->
+                  <AppInput styles="is-rounded is-medium" label="Name" placeholder="Your Name" />
+                </div>
+                <!-- 2 field -->
+                <div class="column is-6">
+                  <AppInput styles="is-rounded is-medium" label="Email" placeholder="Your Email" />
+                </div>
+                <!-- 3 field -->
+                <div class="column is-12">
+                  <AppInput
+                    styles="is-rounded is-medium"
+                    label="Phone"
+                    placeholder="Your Phone Number"
+                  />
+                </div>
+                <!-- 4 field -->
+                <div class="column is-12">
+                  <AppInput
+                    textarea
+                    styles="is-rounded is-medium"
+                    label="Message"
+                    placeholder="Type Your Message Number"
+                  />
+                  <div class="field py-30">
+                    <div class="control">
+                      <button type="submit" class="btn btn-green btn-rounded btn-large">Send Message</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <!-- 2 column -->
+          <div class="column is-5 is-offset-1">
+            <div class="notification has-background-green py-30 px-32">
+              <h2 class="display-2 has-text-white has-text-weight-medium mb-20">Contact Info</h2>
+              <p
+                class="has-text-white mb-12 has-text-weight-medium"
+                v-for="(info, i) in contactInfo"
+                :key="i"
+              >
+                <span class="pr-16 subtitle is-5">
+                  <i :class="info.icon"></i>
+                </span>
+                {{ info.text }}
+              </p>
+
+              <div class="map image pt-12">
+                <img src="@/assets/contact-us-map.png" alt="map" class="responsive-img" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Clients -->
+    <section class="contact-clients pt-30">
+      <div class="container">
+        <AppHeadingTitle title="They Trust Me" />
+        <AppSlider :img="clients">
+          <template v-slot:item="item">
+            <img :src="item.item" />
+          </template>
+        </AppSlider>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import AppHero from "@/components/AppHero";
+import AppInput from "@/components/AppInput";
+import AppSlider from "@/components/AppSlider";
+import AppHeadingTitle from "@/components/AppHeadingTitle";
 
 export default {
   name: "ContactUs",
   components: {
-    AppHero
+    AppHero,
+    AppInput,
+    AppSlider,
+    AppHeadingTitle
+  },
+  data() {
+    return {
+      contactInfo: [
+        { icon: "fa fa-phone", text: "+998998594175" },
+        { icon: "fa fa-envelope", text: "auden.05@mail.ru" },
+        {
+          icon: "fa fa-map-marker",
+          text: "Taskkent, Bekabad Muqumiy 13. 550110 UZB"
+        }
+      ],
+      // Our clients
+      clients: [
+        require("@/assets/clients-img1.png"),
+        require("@/assets/clients-img5.png"),
+        require("@/assets/clients-img6.png"),
+        require("@/assets/clients-img4.png"),
+        require("@/assets/clients-img2.png"),
+        require("@/assets/clients-img3.png")
+      ]
+    };
   }
 };
 </script>
@@ -42,7 +145,12 @@ export default {
 #contact-us-page {
   position: relative;
 
-  .contact-showcase {
+  .contact-clients {
+    padding-bottom: 100px;
+    img {
+      width: auto;
+      margin: 0 auto;
+    }
   }
 }
 </style>

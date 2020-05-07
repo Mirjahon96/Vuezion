d0cf8c5c833d986a38361a6b5cd0911069f6a918
Mirjahon96 <auden.05@mail.ru>
2020-05-08 00:42:39 +0500

    working with blog page

---
+++ src/components/AppCommentBox.vue
@@ -0,0 +1,91 @@
+<template functional>
+  <div id="comment-box">
+    <div class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img
+            :src="props.avatar"
+            :style="{ width: props.avatarSize }"
+            :class="{ 'is-rounded-img' : props.rounded  }"
+          />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div>
+          <h5 class="display-5 has-text-weight-medium mb-8">{{ props.title }}</h5>
+          <a href="#!" class="is-subtitle-1" :class="{ 'is-hoverless': props.hoverlessLink}">
+            <i class="fas fa-calendar has-text-primary"></i>
+            {{ props.subtitle }}
+          </a>
+          <p class="is-subtitle-1 mt-20">{{ props.text }}</p>
+        </div>
+      </div>
+      <div class="media-right" v-if="reply">
+        <a href="#!">Reply</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  functional: true,
+  name: "AppCommentBox",
+  props: {
+    avatar: {
+      type: String,
+      required: false,
+      default: require("@/assets/blog-comment-1.jpg")
+    },
+    avatarSize: {
+      type: String,
+      required: false,
+      default: "90px"
+    },
+    rounded: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    title: {
+      type: String,
+      required: false
+    },
+    subtitle: {
+      type: String,
+      required: false,
+      default: "May 10, 2020 at 12:00 pm"
+    },
+    text: {
+      type: String,
+      required: false,
+      default:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam."
+    },
+    hoverlessLink: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    reply: {
+      type: Boolean,
+      require: false,
+      default: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#comment-box {
+  a {
+    color: inherit;
+    &:hover {
+      color: $primary-color;
+    }
+  }
+  p {
+    line-height: 2;
+  }
+}
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -42,9 +42,105 @@
           </div>
           <!-- 2 -->
           <div class="column is-7">
-            <div class="image">
-              <img src="@/assets/blog-img1.jpg" alt="blog img" class="responsive-img" />
+            <div class="image mb-50">
+              <img src="@/assets/blog-page-img.jpg" alt="blog img" class="responsive-img" />
             </div>
+            <div class="mb-40">
+              <span
+                class="has-background-green has-text-white has-text-weight-bold p-12 mr-12"
+              >Fashion</span>
+              <span class="has-background-green has-text-white has-text-weight-bold p-12">Style</span>
+            </div>
+            <div class="text-content">
+              <h5 class="display-3 has-text-weight-bold">Established fact that a reader will be</h5>
+              <p class="is-subtitle-1 pt-8">
+                By:
+                <a href="#!">Admin</a>
+                |
+                <span>May 10, 2020</span>
+              </p>
+            </div>
+            <div class="my-30">
+              <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Pri quas audiam virtute ut, case utamur fuisset eam ut, iisque accommodare an eam. Reque blandit qui eu, cu vix nonumy volumus. Legendos intellegam id usu, vide oporteat vix eu, id illud principes has. Nam tempor utamur gubergren no.</p>
+            </div>
+            <div class="has-text-centered">
+              <h5
+                class="display-5 has-text-weight-bold"
+              >There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h5>
+            </div>
+
+            <!-- 2.1 -->
+            <div class="columns my-30">
+              <div class="column">
+                <img src="@/assets/blog-img1.jpg" alt="blog img" />
+              </div>
+              <div class="column">
+                <img src="@/assets/blog-img2.jpg" alt="blog img" />
+              </div>
+              <div class="column">
+                <img src="@/assets/blog-img3.jpg" alt="blog img" />
+              </div>
+            </div>
+            <div class="mb-30">
+              <h5 class="display-3 has-text-weight-medium">Established fact that a reader will be</h5>
+              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            </div>
+            <!-- comment box -->
+            <article class="comment-box">
+              <AppCommentBox
+                avatarSize="150px"
+                title="Rinks Kevin"
+                text="CEO & It was popularised in the 1960s with the and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                reply
+              ></AppCommentBox>
+              <div class="mt-40">
+                <h5 class="display-5 has-text-weight-bold">
+                  Comments
+                  <span class="subtitle is-6 has-text-weight-medium">/ 21</span>
+                </h5>
+
+                <hr class="my-4" />
+                <div class="comment-item" v-for="(comment, index) in comments" :key="index">
+                  <AppCommentBox
+                    :avatar="comment.img"
+                    :title="comment.title"
+                    :subtitle="comment.subtitle"
+                    text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam."
+                    hoverlessLink
+                  ></AppCommentBox>
+                </div>
+              </div>
+
+              <!-- Post Comment -->
+              <form class="post-comment">
+                <h5 class="display-5 has-text-weight-bold">Post A Comment</h5>
+                <div class="columns">
+                  <div class="column" v-for="(post, index) in postComments" :key="index">
+                    <AppInput
+                      styles="is-rounded is-medium"
+                      :label="post.label"
+                      :placeholder="post.placeholder"
+                    />
+                  </div>
+                </div>
+                <div class="columns">
+                  <div class="column">
+                    <AppInput
+                      textarea
+                      styles="is-rounded is-medium"
+                      label="Comment"
+                      placeholder="Comment*"
+                    />
+                    <div class="field py-30">
+                      <div class="control">
+                        <button type="submit" class="btn btn-green btn-rounded btn-large">Send Post</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <!-- end post comment -->
+            </article>
           </div>
         </div>
       </div>
@@ -55,12 +151,16 @@
 <script>
 import AppHero from "@/components/AppHero";
 import AppIcon from "@/components/AppIcon";
+import AppCommentBox from "@/components/AppCommentBox";
+import AppInput from "@/components/AppInput";
 
 export default {
   name: "Blog",
   components: {
     AppHero,
-    AppIcon
+    AppIcon,
+    AppCommentBox,
+    AppInput
   },
   data() {
     return {
@@ -68,6 +168,29 @@ export default {
         { name: "fab fa-facebook-f", href: "#!" },
         { name: "fab fa-twitter", href: "#!" },
         { name: "fab fa-google", href: "#!" }
+      ],
+      // Comments
+      comments: [
+        {
+          img: require("@/assets/blog-comment-4.jpg"),
+          title: "Kevin Martin",
+          subtitle: "May 9, 2020 at 14:14 am"
+        },
+        {
+          img: require("@/assets/blog-comment-3.jpg"),
+          title: "John Foe",
+          subtitle: "May 9, 2020 at 9:57 am"
+        },
+        {
+          img: require("@/assets/blog-comment-2.jpg"),
+          title: "Lora Jenkins",
+          subtitle: "May 8, 2020 at 1:40 pm"
+        }
+      ],
+      postComments: [
+        { label: "Name", placeholder: "Your Name*" },
+        { label: "Email", placeholder: "Your Email*" },
+        { label: "Phone", placeholder: "Your No*" }
       ]
     };
   }
@@ -80,6 +203,7 @@ export default {
 
   // Content
   .blog-content {
+    padding-bottom: 100px;
     .badge {
       border-radius: 50%;
       border: none;
@@ -87,6 +211,17 @@ export default {
       height: 80px;
       width: 80px;
       padding-top: 10px;
+    }
+
+    .comment-box {
+      border: 1px solid #e4e4e4;
+      border-radius: 8px;
+      padding: 30px 20px 10px 20px;
+    }
+    .comment-item {
+      padding: 20px;
+      border: 1px solid #e7e7e7;
+      margin-bottom: 20px;
     }
   }
 }
